8187f06cff8ad7b8dfd5e5d50164aab3c1511cd9
Santanu Karmakar <santanu.karmakar@quantuminventions.com>
2018-01-18 13:51:48 +0530

    Fix missing env in the message

--- lib/exception_notifier/team_webhook_notifier.rb
+++ lib/exception_notifier/team_webhook_notifier.rb
@@ -8,22 +8,24 @@ module ExceptionNotifier
 
     # very basic team webhook notifier
     def call(exception, options={})
-      env = options[:env]
-
       options = options.reverse_merge(@default_options)
+      error_precedence = "[#{options[:env]} Error]: "
+
       url = options.delete(:url)
       http_method = :post
 
       uri = URI.parse(url)
       req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
 
-      message = if options[:simplified]
-                  "[#{env} Error]: " + (exception.is_a?(String) ? exception : exception.backtrace.reject{|l| l =~ %r|\A[^:]*/gems/|}.join("\n"))
+      message = if exception.is_a?(String)
+                  exception
+                elsif options[:simplified]
+                  exception.backtrace.reject{|l| l =~ %r|\A[^:]*/gems/|}.join("\n")
                 else
-                  "[#{env} Error]: " + (exception.is_a?(String) ? exception : exception.backtrace.join("\n"))
+                  exception.backtrace.join("\n")
                 end
 
-      req.body = { text: message }.to_json
+      req.body = { text: error_precedence + message }.to_json
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
